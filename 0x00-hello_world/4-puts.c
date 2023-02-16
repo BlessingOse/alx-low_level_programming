8f9918dd92ee514aaa28a414f93b109dd1c86145
BlessingOse <maryblessing633@gmail.com>
2023-02-16 06:13:14 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the main fuction
+ * Return: this returns 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	Return(0);
+}
