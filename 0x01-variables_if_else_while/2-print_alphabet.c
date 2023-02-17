0f5bd56bc9b8b89d8e7e4d40c92a3e7f1d0da18f
BlessingOse <maryblessing633@gmail.com>
2023-02-17 05:02:49 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - all starts here
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'a' ; n <= 'z' ; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
