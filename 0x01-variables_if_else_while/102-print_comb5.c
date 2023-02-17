3e96637d05c713de1c6ea1f9c6fe83ee267a3c4f
BlessingOse <maryblessing633@gmail.com>
2023-02-17 11:02:16 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - it all starts here
+ *
+ * Return: to return 0
+ */
+
+int main(void)
+{
+
+	int m;
+	int n;
+
+	for (m = 0 ; m <= 98 ; m++)
+	{
+		for (n = m + 1 ; n <= 99 ; n++)
+		{
+			putchar((m / 10) + '0');
+			putchar((m % 10) + '0');
+			putchar(' ');
+			putchar((n / 10) + '0');
+			putchar((n % 10) + '0');
+			if (m == 98 && n == 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
