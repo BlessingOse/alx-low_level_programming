1d443567dfa63d798dee4a3e173c6bf8e9030cfc
BlessingOse <maryblessing633@gmail.com>
2023-03-01 05:04:04 -0800

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *rot13 - encodes a string using rot13
+ * @s: string
+ *
+ * Return: the result
+ */
+
+char *rot13(char *s)
+{
+	int m, n;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (m = 0; s[m] != '\0'; m++)
+	{
+		for (n = 0; a[n] != '\0'; n++)
+		{
+			if (s[m] == a[n])
+			{
+				s[m] = b[n];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
