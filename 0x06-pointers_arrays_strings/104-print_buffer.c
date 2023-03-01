6b6b01c1d523ddd6651be44281bb7cccc02870da
BlessingOse <maryblessing633@gmail.com>
2023-03-01 09:43:21 -0800

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,3 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - prints a s bytes of a buffer
+ * @c: buffer
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
+ *
+ * Return: 0
+ */
+
+void print_line(char *c, int s, int l)
+{
+int a, b;
+for (a = 0; a <= 9; a++)
+{
+if (a <= s)
+printf("%02x", c[l * 10 + a]);
+else
+printf("  ");
+if (a % 2)
+putchar(' ');
+}
+for (b = 0; b <= s; b++)
+{
+if (c[l * 10 + b] > 31 && c[l * 10 + b] < 127)
+putchar(c[l * 10 + b]);
+else
+putchar('.');
+}
+}
+
 #include <stdio.h>
 #include "main.h"
 
@@ -17,11 +50,11 @@ for (i = 0; i <= (size - 1) / 10 && size; i++)
 printf("%08x: ", i * 10);
 if (i < size / 10)
 {
-printf(b, 9, i);
+print_line(b, 9, i);
 }
 else
 {
-printf(b, size % 10 - 1, i);
+print_line(b, size % 10 - 1, i);
 }
 putchar('\n');
 }
