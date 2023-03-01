9483d285ae57fb7ccc091d727677d93b73b3821c
BlessingOse <maryblessing633@gmail.com>
2023-03-01 04:38:59 -0800

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *leet - encodes a string into 1337
+ * @s: string
+ *
+ * Return: the result
+ */
+
+char *leet(char *s)
+{
+	int m, n;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (m = 0; s[m] != '\0'; m++)
+	{
+		for (n = 0; n < 10; n++)
+		{
+			if (s[m] == a[n])
+			{
+				s[n] = b[n];
+			}
+		}
+	}
+
+	return (s);
+}
