01cd75cfc9e764df2e8a8854c35754583ac4fb32
BlessingOse <maryblessing633@gmail.com>
2023-09-12 02:56:29 -0700

    fixed

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -28,7 +28,7 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 		k++;
 		index = k * m;
 
-		while (list->next && list->ind < index)
+		while (list->next && list->index < index)
 			list = list->next;
 
 		if (list->next == NULL && index != list->index)
